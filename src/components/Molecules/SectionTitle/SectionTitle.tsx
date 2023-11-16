25f35e9213fe241809515728f1f13baaf8926086
Christopher <christopher.cudiamat@jagaad.com>
2023-11-16 18:46:12 +0100

    feat: create section-title component

---
+++ src/components/Molecules/SectionTitle/SectionTitle.tsx
@@ -0,0 +1,34 @@
+import Container from '@/components/Atoms/Container/Container';
+import Link from 'next/link';
+import React from 'react'
+
+interface SectionTitleProps {
+  title: string;
+  ButtonLabel?: string;
+  href?: string;
+}
+
+const SectionTitle = ({
+  title,
+  ButtonLabel,
+  href,
+}: SectionTitleProps) => {
+  return (
+    <Container className="flex flex-col lg:flex-row items-center lg:justify-between text-center lg:text-left mb-6 lg:mb-8">
+      <h2 className="text-slate-600 text-3xl font-semibold mb-5 lg:mb-0">
+        {title}
+      </h2>
+      {
+        (ButtonLabel && href) &&
+        <Link 
+          href={href} 
+          className="bg-blue-600 font-semibold text-white px-4 py-2 w-fit rounded-md"
+        >
+          {ButtonLabel}
+        </Link>
+      }
+    </Container>
+  )
+}
+
+export default SectionTitle
