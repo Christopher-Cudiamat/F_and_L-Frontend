51eecd11a307362d6ed2e595bdeb5dc4168517e5
unknown <christophervcudiamat.13@gmail.com>
2023-11-23 01:14:26 +0800

    style: change view all button size and color

--- src/components/Molecules/SectionTitle/SectionTitle.tsx
+++ src/components/Molecules/SectionTitle/SectionTitle.tsx
@@ -1,4 +1,5 @@
 import Container from '@/components/Atoms/Container/Container';
+import { ArrowLongRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import React from 'react'
 
@@ -17,17 +18,18 @@ const SectionTitle = ({
 }: SectionTitleProps) => {
   return (
     <Container className={`${className}`}>
-      <div className="flex flex-col lg:flex-row items-center lg:justify-between text-center lg:text-left mb-6 lg:mb-8">
-        <h2 className="text-slate-600 text-3xl lg:text-4xl font-semibold mb-5 lg:mb-0">
+      <div className="flex flex-col md:flex-row items-center md:justify-between text-center lg:text-left mb-6 lg:mb-8">
+        <h2 className="text-slate-600 text-3xl lg:text-4xl font-semibold mb-5 md:mb-0">
           {title}
         </h2>
         {
           (ButtonLabel && href) &&
           <Link 
             href={href} 
-            className="bg-blue-600 hover:bg-blue-700 font-semibold text-white px-4 py-2 w-fit rounded-md duration-200"
+            className="flex items-center gap-x-2 font-semibold text-xl text-slate-600 w-fit border-b-2 border-slate-600/0 hover:border-slate-600 duration-200"
           >
             {ButtonLabel}
+            <ArrowLongRightIcon className='w-10' />
           </Link>
         }
       </div>
